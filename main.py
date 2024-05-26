428a9e285706609d31893b7736e3cc21e8ca0213
David <daviddasilvarodrigues97@gmail.com>
2024-05-26 12:26:26 -0300

    criacao da classe personagem e adicionado movimentos

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 import pygame, sys
+from pygame.locals import *
+from sys import exit
+from personagem.personagem import Personagem
 
 pygame.init()
 
-scr = pygame.display.set_mode((500,500))
+altura = 500
+largura = 500
+
+scr = pygame.display.set_mode((largura,altura))
 pygame.display.set_caption("EDL GAME")
 
+player = Personagem(50, 50, 50, 50, (0, 0, 255))
+
 while True:
+    scr.fill((0,0,0))
     for ev in pygame.event.get():
         if ev.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         if ev.type == pygame.KEYDOWN:
             key = pygame.key.name(ev.key)
-            print(key," tecla Pressionada")
+            print(key, "tecla Pressionada")
         if ev.type == pygame.MOUSEMOTION:
             pos = pygame.mouse.get_pos()
             # btn = pygame.mouse.get_pressed()
-            print("x {},y {}".format(pos[0],pos[1]))
+            print("x {},y {}".format(pos[0], pos[1]))
 
+    keys = pygame.key.get_pressed()
+    player.move(keys)
+    player.draw(scr)
 
-
-
+    pygame.display.update()
---
+++ personagem/personagem.py
@@ -0,0 +1,23 @@
+import pygame
+from pygame.locals import *
+
+class Personagem:
+    def __init__(self, x, y, largura, altura, cor):
+        self.largura = largura
+        self.altura = altura
+        self.x = x
+        self.y = y
+        self.cor = cor
+
+    def draw(self, scr):
+        pygame.draw.rect(scr, self.cor, (self.x, self.y, 50, 50))
+    
+    def move(self, keys):
+        if keys[K_a] or keys[pygame.K_LEFT]:
+            self.x -= 0.25
+        if keys[K_d] or keys[pygame.K_RIGHT]:
+            self.x += 0.25
+        if keys[K_w] or keys[pygame.K_UP]:
+            self.y -= 0.25
+        if keys[K_s] or keys[pygame.K_DOWN]:
+            self.y += 0.25
