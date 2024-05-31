956923e173c67c2de83a590da162bebd4c9681fb
TiagoSenaD <tiagosenaduraes@gmail.com>
2024-05-31 16:45:52 -0300

    criação da classepra criar os campos. Ela ainda é um prototipo, mas já esta imprimindo na tela os tiles. TODO: redimensionalizar o campo e fazer a classe estagio que vai realmete herdar dessa classe

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import pygame, sys
-from stage import grids
+from stage import *
 
 pygame.init()
 
 
 #Definoções da tela
 largura = 1000
-altura = 1000
+altura = 800
 
 
 scr = pygame.display.set_mode((largura,altura))
 pygame.display.set_caption("EDL GAME")
 
+stage = stage(scr, altura, largura)
+
 while True:
     scr.fill((1,1,0))
+    stage.draw()
     grids(scr,altura,largura)
     for ev in pygame.event.get():
         if ev.type == pygame.QUIT:
@@ -26,7 +29,7 @@ while True:
             # pos = pygame.mouse.get_pos()
             # btn = pygame.mouse.get_pressed()
             # print("x {},y {}".format(pos[0],pos[1]))
-
+    pygame.display.update()
 
 
 
